3c2b6c2446d0cdf294d3aa8a37ebcb34344df1cb
joshibasu62 <joshibasu62@gmail.com>
2025-12-25 12:08:53 +0545

    first commit

---
+++ src/px4_keyboard_offboard/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'px4_keyboard_offboard'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='basanta-joshi',
+    maintainer_email='joshibasu62@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'px4_keyboard_offboard = px4_keyboard_offboard.offboard_keyboard:main',
+            'control = px4_keyboard_offboard.control:main'
+        ],
+    },
+)
