c7151ca0d2d32cb17c09f1f42afd525879174474
joshibasu62 <joshibasu62@gmail.com>
2026-03-15 16:13:48 +0545

    for pi1

--- src/CSLAM-UAV/drone_slam_pkg/launch/rpi_launch_oakd.launch.py
+++ src/CSLAM-UAV/drone_slam_pkg/launch/rpi_launch_oakd.launch.py
@@ -40,10 +40,10 @@ def generate_launch_description():
     return LaunchDescription([
         
         
-        ExecuteProcess(
-            cmd=['MicroXRCEAgent', 'udp4', '--port', '8888'],
-            output='screen'
-        ),
+        # ExecuteProcess(
+        #     cmd=['MicroXRCEAgent', 'udp4', '--port', '8888'],
+        #     output='screen'
+        # ),
 
         Node(
             package='drone_slam_pkg', 
@@ -53,14 +53,14 @@ def generate_launch_description():
         ),
 
         
-        # Node(
-        #     package='depthai_ros_driver',
-        #     executable='camera_node',
-        #     name='oak_camera',
-        #     output='screen',
-        #     parameters=[os.path.expanduser('~/oak_run.yaml')],
-        # ),
-        #oak_rgb_camera_optical_frame
+        Node(
+            package='depthai_ros_driver',
+            executable='camera_node',
+            name='oak_camera',
+            output='screen',
+            parameters=[os.path.expanduser('~/oak_run.yaml')],
+        ),
+        oak_rgb_camera_optical_frame
 
         Node(
             package='tf2_ros',
@@ -118,35 +118,35 @@ def generate_launch_description():
             arguments=['-d'], 
         ),
 
-        Node(
-            package='rtabmap_viz',
-            executable='rtabmap_viz',
-            name='rtabmap_viz',
-            namespace='rtabmap',
-            output='screen', 
-            parameters=[vslam_params],
-            remappings=[
-                ("imu", "/imu/data_converted"),
-                ('odom', '/rtabmap/odom'),
-            ],
-        ),
+        # Node(
+        #     package='rtabmap_viz',
+        #     executable='rtabmap_viz',
+        #     name='rtabmap_viz',
+        #     namespace='rtabmap',
+        #     output='screen', 
+        #     parameters=[vslam_params],
+        #     remappings=[
+        #         ("imu", "/imu/data_converted"),
+        #         ('odom', '/rtabmap/odom'),
+        #     ],
+        # ),
 
-        Node(
-            package='px4_ros_com', 
-            executable='ros_odometry_to_vehicle_odometry_wo_map', 
-            name='ros_odometry_to_vehicle_odometry_wo_map',
-            output='screen',
-            parameters=[{
-                'use_sim_time': use_sim_time,
-                'repeat_odom': True,
-                            }]
-        ),
+        # Node(
+        #     package='px4_ros_com', 
+        #     executable='ros_odometry_to_vehicle_odometry_wo_map', 
+        #     name='ros_odometry_to_vehicle_odometry_wo_map',
+        #     output='screen',
+        #     parameters=[{
+        #         'use_sim_time': use_sim_time,
+        #         'repeat_odom': True,
+        #                     }]
+        # ),
 
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            output='screen',
-            arguments=['-d', '/home/basanta-joshi/Desktop/cslam/src/CSLAM-UAV/drone_slam_pkg/rviz/drone_0.rviz'],
-            parameters=[{'use_sim_time': use_sim_time}]
-        ),
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     output='screen',
+        #     arguments=['-d', '/home/basanta-joshi/Desktop/cslam/src/CSLAM-UAV/drone_slam_pkg/rviz/drone_0.rviz'],
+        #     parameters=[{'use_sim_time': use_sim_time}]
+        # ),
     ])
